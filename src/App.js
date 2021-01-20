712e04869de6a6c0ca26c73cbbf330c136de1e56
Сергей Мартынов <s1rgik@ya.ru>
2021-01-20 14:29:23 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import React from 'react';
+import Axios from 'axios';
+
+import UsersList from './components/UsersList';
+
 function App() {
-  return <div className="App"></div>;
+  const [inputValue, setInputValue] = React.useState('');
+  const [users, setUsers] = React.useState([]);
+  const [payload, setPayload] = React.useState(false);
+  const [page, setPage] = React.useState(1);
+
+  const limit = 10;
+
+  React.useEffect(() => {
+    setPayload('loading');
+    Axios.get(`https://5c3755177820ff0014d92711.mockapi.io/users?page=${page}&limit=${limit}`)
+      .then(({ data }) => {
+        setUsers((u) => [...u, ...data]);
+        data.length ? setPayload('loaded') : setPayload('max');
+      })
+      .catch((err) => {
+        setPayload('failed');
+        alert('Ошибка при получении пользователей');
+      });
+  }, [page]);
+
+  const nextPage = (e) => {
+    if (payload !== 'max') {
+      setPage(page + 1);
+    }
+  };
+
+  return (
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Поиск пользователя..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {users.length ? <UsersList users={users} filter={inputValue} /> : 'Loading...'}
+      {users.length && payload !== 'max' && payload !== 'failed' ? (
+        <button onClick={nextPage} disabled={payload === 'loading' || payload === 'failed'}>
+          {payload === 'loading' ? 'Wait...' : null}
+          {payload === 'loaded' ? 'Next 10 users' : null}
+        </button>
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
