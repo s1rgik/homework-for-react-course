cca4288c4094a118b1b9cb5253167ccace48175f
Сергей Мартынов <s1rgik@ya.ru>
2021-01-20 14:31:50 +0300

    Server search implementation

--- src/App.js
+++ src/App.js
@@ -4,42 +4,79 @@ import Axios from 'axios';
 import UsersList from './components/UsersList';
 
 function App() {
-  const [inputValue, setInputValue] = React.useState('');
   const [users, setUsers] = React.useState([]);
   const [payload, setPayload] = React.useState(false);
-  const [page, setPage] = React.useState(1);
+  const [inputValue, setInputValue] = React.useState('');
+  const [query, setQuery] = React.useState('page=1');
+  const page = React.useRef(1);
+  const limit = React.useRef(10);
+  const baseUrl = 'https://5c3755177820ff0014d92711.mockapi.io/users';
 
-  const limit = 10;
+  let typingTimer = React.useRef();
 
   React.useEffect(() => {
     setPayload('loading');
-    Axios.get(`https://5c3755177820ff0014d92711.mockapi.io/users?page=${page}&limit=${limit}`)
+    Axios.get(`${baseUrl}?limit=${limit.current}&${query}`)
       .then(({ data }) => {
-        setUsers((u) => [...u, ...data]);
-        data.length ? setPayload('loaded') : setPayload('max');
+        if (page.current === 1) {
+          setUsers(data);
+        } else {
+          setUsers((users) => [...users, ...data]);
+        }
+
+        setPayload('loaded');
+
+        if (data.length < limit.current) {
+          setPayload('max');
+        }
+
+        if (!data.length && page.current === 1) {
+          setPayload('notFound');
+        }
       })
       .catch((err) => {
         setPayload('failed');
         alert('Ошибка при получении пользователей');
       });
-  }, [page]);
+  }, [query]);
 
-  const nextPage = (e) => {
+  const nextPage = () => {
     if (payload !== 'max') {
-      setPage(page + 1);
+      ++page.current;
+      setQueryParameters(inputValue, page.current);
     }
+  };
+
+  const search = (searchStr) => {
+    setInputValue(searchStr);
+    setUsers([]);
+    clearTimeout(typingTimer.current);
+    page.current = 1;
+
+    typingTimer.current = setTimeout(() => {
+      setQueryParameters(searchStr, page.current);
+    }, 500);
+  };
+
+  const setQueryParameters = (searchStr = '', page = 1) => {
+    let searchParameters = searchStr ? `name=${searchStr}&page=${page}` : `page=${page}`;
+    setQuery(searchParameters);
   };
 
   return (
     <div className="app">
       <input
         type="text"
-        placeholder="Поиск пользователя..."
+        placeholder="Поиск пользователя по имени и фамилии..."
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={(e) => search(e.target.value)}
       />
-      {users.length ? <UsersList users={users} filter={inputValue} /> : 'Loading...'}
-      {users.length && payload !== 'max' && payload !== 'failed' ? (
+
+      {payload === 'notFound' ? 'нет пользователей' : null}
+      {!users.length && payload !== 'notFound' ? 'Loading...' : null}
+      {users.length ? <UsersList users={users} /> : null}
+
+      {users.length && (payload === 'loading' || payload === 'loaded' || payload === 'failed') ? (
         <button onClick={nextPage} disabled={payload === 'loading' || payload === 'failed'}>
           {payload === 'loading' ? 'Wait...' : null}
           {payload === 'loaded' ? 'Next 10 users' : null}
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,18 +1,12 @@
 const UsersList = ({ users, filter }) => {
   return (
     <ul className="users">
-      {users
-        .filter(
-          (obj) =>
-            obj.name.toLowerCase().includes(filter.toLowerCase()) ||
-            obj.email.toLowerCase().includes(filter.toLowerCase()),
-        )
-        .map((obj) => (
-          <li key={obj.id} className="users__item">
-            <div className="users__item-name">{obj.name}</div>
-            <div className="users__item-email">{obj.email}</div>
-          </li>
-        ))}
+      {users.map((obj) => (
+        <li key={obj.id} className="users__item">
+          <div className="users__item-name">{obj.name}</div>
+          <div className="users__item-email">{obj.email}</div>
+        </li>
+      ))}
     </ul>
   );
 };
